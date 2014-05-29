01184736c991c285e02f5315043851995f20c057
Brian Di Palma <offler@gmail.com>
2014-05-29 10:02:49 +0100

    Attach the HeaderElement to the Application.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import {HeaderElement} from "spa-2014-header";
 
 document.registerElement("spa2014-header", HeaderElement);
 
-export function printHello() {
-	console.info("Hello");
+export function insertApplicationElements() {
+	document.body.innerHTML = "<spa2014-header></spa2014-header>";
 }
