e0fd704be3842f2cabca0fa73d58626268fd25a4
Brian Di Palma <offler@gmail.com>
2014-05-30 17:54:08 +0100

    Add spa-2014-recent-messages to the spa 2014 app.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import {HeaderElement} from "spa-2014-header";
+import {RecentMessagesElement} from "spa-2014-recent-messages";
 
 document.registerElement("spa2014-header", HeaderElement);
+document.registerElement("spa2014-recent-messages", RecentMessagesElement);
 
 export function insertApplicationElements() {
-	document.body.innerHTML = "<spa2014-header></spa2014-header>";
+	document.body.innerHTML = "<spa2014-header></spa2014-header>\
+								<spa2014-recent-messages></spa2014-recent-messages>";
 }
