e23bcac75a33523328d7273cb26690da63c49988
Brian Di Palma <offler@gmail.com>
2014-06-01 18:45:43 +0100

    Add ChatBox to SPA application.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import {HeaderElement} from "spa-2014-header";
+import {ChatBoxElement} from "spa-2014-chat-box";
 import {RecentMessagesElement} from "spa-2014-recent-messages";
 
 document.registerElement("spa2014-header", HeaderElement);
+document.registerElement("spa2014-chat-box", ChatBoxElement);
 document.registerElement("spa2014-recent-messages", RecentMessagesElement);
 
 export function insertApplicationElements() {
 	document.body.innerHTML = "<spa2014-header></spa2014-header>\
+								<spa2014-chat-box></spa2014-chat-box>\
 								<spa2014-recent-messages></spa2014-recent-messages>";
 }
