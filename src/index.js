2890b9ba36750cd0f3e2dc92fe83d51b65d7ccec
Brian Di Palma <offler@gmail.com>
2014-06-02 11:03:33 +0100

    Add chat-panel to the spa-app.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import {HeaderElement} from "spa-2014-header";
 import {ChatBoxElement} from "spa-2014-chat-box";
+import {ChatPanelElement} from "spa-2014-chat-panel";
 import {RecentMessagesElement} from "spa-2014-recent-messages";
 
 document.registerElement("spa2014-header", HeaderElement);
 document.registerElement("spa2014-chat-box", ChatBoxElement);
+document.registerElement("spa-2014-chat-panel", ChatPanelElement);
 document.registerElement("spa2014-recent-messages", RecentMessagesElement);
 
 export function insertApplicationElements() {
 	document.body.innerHTML = "<spa2014-header></spa2014-header>\
+								<spa-2014-chat-panel></spa-2014-chat-panel>\
 								<spa2014-chat-box></spa2014-chat-box>\
 								<spa2014-recent-messages></spa2014-recent-messages>";
 }
