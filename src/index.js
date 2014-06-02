18a2ceb709fa85068dbd4c814a8ff5bd8c4e3e4e
Brian Di Palma <offler@gmail.com>
2014-06-02 12:20:20 +0100

    Use the SpaApplicationElement to display the application.
    Instead of innerHTML'ing into the document body.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import {ChatBoxElement} from "spa-2014-chat-box";
 import {ChatPanelElement} from "spa-2014-chat-panel";
 import {RecentMessagesElement} from "spa-2014-recent-messages";
 
-import "../style/index.css!";
+import {SpaApplicationElement} from "./SpaApplicationElement";
 
 document.registerElement("spa2014-header", HeaderElement);
 document.registerElement("spa2014-chat-box", ChatBoxElement);
+document.registerElement("spa-2014-app", SpaApplicationElement);
 document.registerElement("spa-2014-chat-panel", ChatPanelElement);
 document.registerElement("spa2014-recent-messages", RecentMessagesElement);
 
 export function insertApplicationElements() {
-	document.body.innerHTML = "<spa2014-header></spa2014-header>\
-								<spa-2014-chat-panel></spa-2014-chat-panel>\
-								<spa2014-chat-box></spa2014-chat-box>\
-								<spa2014-recent-messages></spa2014-recent-messages>";
 }
