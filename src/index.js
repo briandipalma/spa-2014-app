0ce79e9d9669fac82f72fa9f21734c2d6f008b4e
Brian Di Palma <offler@gmail.com>
2014-06-02 14:31:37 +0100

    Implement createStoreAndActions.
    Move custom element registration to a module function.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+import {Dispatcher} from "flux-es6";
 import {HeaderElement} from "spa-2014-header";
 import {ChatBoxElement} from "spa-2014-chat-box";
 import {ChatPanelElement} from "spa-2014-chat-panel";
 import {RecentMessagesElement} from "spa-2014-recent-messages";
 
-import {SpaApplicationElement} from "./SpaApplicationElement";
+import SpaApplicationStore from "./SpaApplicationStore";
+import SpaApplicationActions from "./SpaApplicationActions";
+import SpaApplicationElement from "./SpaApplicationElement";
 
-document.registerElement("spa2014-header", HeaderElement);
-document.registerElement("spa2014-chat-box", ChatBoxElement);
-document.registerElement("spa-2014-app", SpaApplicationElement);
-document.registerElement("spa-2014-chat-panel", ChatPanelElement);
-document.registerElement("spa2014-recent-messages", RecentMessagesElement);
+export function registerApplicationElements() {
+	document.registerElement("spa2014-header", HeaderElement);
+	document.registerElement("spa2014-chat-box", ChatBoxElement);
+	document.registerElement("spa-2014-app", SpaApplicationElement);
+	document.registerElement("spa-2014-chat-panel", ChatPanelElement);
+	document.registerElement("spa2014-recent-messages", RecentMessagesElement);
+}
 
-export function insertApplicationElements() {
+export function createStoreAndActions() {
+	var spaApplicationDispatcher = new Dispatcher();
+	var spaApplicationStore = new SpaApplicationStore();
+	var spaApplicationActions = new SpaApplicationActions(spaApplicationDispatcher);
+
+	spaApplicationDispatcher.register((payload) => spaApplicationStore.handleDispatcherAction(payload));
+
+	return [spaApplicationStore, spaApplicationActions];
 }
