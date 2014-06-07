32c2f6a11733fbdf53fe5b3f95f3f3487c3252c8
Brian Di Palma <offler@gmail.com>
2014-06-07 23:28:25 +0100

    Use `createStoreAndActions` from the flux-es6 package.

--- src/SpaApplicationElement.js
+++ src/SpaApplicationElement.js
@@ -1,4 +1,7 @@
-import {createStoreAndActions} from "./index";
+import {createStoreAndActions} from "flux-es6";
+
+import SpaApplicationStore from "./SpaApplicationStore";
+import SpaApplicationActions from "./SpaApplicationActions";
 
 import "../style/index.css!";
 import spaApplicationTemplate from "../template/spaApplication.text!";
@@ -6,7 +9,7 @@ import spaApplicationTemplate from "../template/spaApplication.text!";
 export default class extends HTMLElement {
 	// Fires when an instance of the SpaApplicationElement is created
 	createdCallback() {
-		var [spaApplicationStore, spaApplicationActions] = createStoreAndActions();
+		var [spaApplicationStore, spaApplicationActions] = createStoreAndActions(SpaApplicationStore, SpaApplicationActions);
 
 		this.innerHTML = spaApplicationTemplate;
 		this.spaApplicationStore = spaApplicationStore;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import {Dispatcher} from "flux-es6";
 import {HeaderElement} from "spa-2014-header";
 import {ChatBoxElement} from "spa-2014-chat-box";
 import {ChatPanelElement} from "spa-2014-chat-panel";
 import {RecentMessagesElement} from "spa-2014-recent-messages";
 
-import SpaApplicationStore from "./SpaApplicationStore";
-import SpaApplicationActions from "./SpaApplicationActions";
 import SpaApplicationElement from "./SpaApplicationElement";
 
 export function registerApplicationElements() {
@@ -14,14 +11,4 @@ export function registerApplicationElements() {
 	document.registerElement("spa-2014-chat-panel", ChatPanelElement);
 	document.registerElement("spa2014-recent-messages", RecentMessagesElement);
 	document.registerElement("spa-2014-app", SpaApplicationElement);
-}
-
-export function createStoreAndActions() {
-	var spaApplicationDispatcher = new Dispatcher();
-	var spaApplicationStore = new SpaApplicationStore();
-	var spaApplicationActions = new SpaApplicationActions(spaApplicationDispatcher);
-
-	spaApplicationDispatcher.register((payload) => spaApplicationStore.handleDispatcherAction(payload));
-
-	return [spaApplicationStore, spaApplicationActions];
 }
