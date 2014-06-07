0d40f7555d00d870afad4d44fd4cdbba887a8f6b
Brian Di Palma <offler@gmail.com>
2014-06-07 22:44:51 +0100

    Register with the recent messages dispatcher.
    Allows the application to know when a user has selected a recent message conversation.

--- src/SpaApplicationStore.js
+++ src/SpaApplicationStore.js
@@ -1,5 +1,6 @@
 import {Store} from 'flux-es6';
 import {HeaderConstants} from 'spa-2014-header';
+import {RecentMessagesConstants} from 'spa-2014-recent-messages';
 
 import SpaApplicationConstants from './SpaApplicationConstants';
 import {CommunicationService} from './communication/CommunicationService';
@@ -26,6 +27,9 @@ export default class extends Store {
 			case HeaderConstants.LOGGED_IN:
 				applicationState.applicationStatus = "";
                 break;
+			case RecentMessagesConstants.MESSAGE_SELECTED:
+				console.log(action.userName);
+                break;
             default:
                 return true;
         }
@@ -40,11 +44,13 @@ export default class extends Store {
 		var recentMessages = applicationElement.querySelector('spa2014-recent-messages');
 		var headerDispatcher = applicationHeader.headerDispatcher;
 		var spaApplicationActions = applicationElement.spaApplicationActions;
+		var recentMessagesDispatcher = recentMessages.recentMessagesDispatcher;
 
 		spaApplicationActions._headerActions = applicationHeader.headerActions;
 		spaApplicationActions._recentMessagesActions = recentMessages.recentMessagesActions;
 
 		headerDispatcher.register((payload) => this.handleDispatcherAction(payload));
+		recentMessagesDispatcher.register((payload) => this.handleDispatcherAction(payload));
 
 		this._communicationService = new CommunicationService(spaApplicationActions);
 	}
