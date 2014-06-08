abd6f7a84379bcc91046e0283056141c837334b8
Brian Di Palma <offler@gmail.com>
2014-06-08 18:53:28 +0100

    Use single quotes instead of double quotes.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import {HeaderElement} from "spa-2014-header";
-import {ChatBoxElement} from "spa-2014-chat-box";
-import {ChatPanelElement} from "spa-2014-chat-panel";
-import {RecentMessagesElement} from "spa-2014-recent-messages";
+import {HeaderElement} from 'spa-2014-header';
+import {ChatBoxElement} from 'spa-2014-chat-box';
+import {ChatPanelElement} from 'spa-2014-chat-panel';
+import {RecentMessagesElement} from 'spa-2014-recent-messages';
 
-import SpaApplicationElement from "./SpaApplicationElement";
+import SpaApplicationElement from './SpaApplicationElement';
 
 export function registerApplicationElements() {
-	document.registerElement("spa2014-header", HeaderElement);
-	document.registerElement("spa2014-chat-box", ChatBoxElement);
-	document.registerElement("spa2014-chat-panel", ChatPanelElement);
-	document.registerElement("spa2014-recent-messages", RecentMessagesElement);
-	document.registerElement("spa2014-app", SpaApplicationElement);
+	document.registerElement('spa2014-header', HeaderElement);
+	document.registerElement('spa2014-chat-box', ChatBoxElement);
+	document.registerElement('spa2014-chat-panel', ChatPanelElement);
+	document.registerElement('spa2014-recent-messages', RecentMessagesElement);
+	document.registerElement('spa2014-app', SpaApplicationElement);
 }
