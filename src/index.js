068ce4d9c69810c89bbe3d19feb4f4a664049356
Brian Di Palma <offler@gmail.com>
2014-06-28 18:39:37 +0100

    Add step4 register contacts-list custom element.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {HeaderElement} from 'spa-2014-header';
 import {ChatBoxElement} from 'spa-2014-chat-box';
 import {ChatPanelElement} from 'spa-2014-chat-panel';
+import {ContactsListElement} from 'spa-2014-contacts-list';
 import {RecentMessagesElement} from 'spa-2014-recent-messages';
 
 import SpaApplicationElement from './SpaApplicationElement';
@@ -9,6 +10,7 @@ export function registerApplicationElements() {
 	document.registerElement('spa2014-header', HeaderElement);
 	document.registerElement('spa2014-chat-box', ChatBoxElement);
 	document.registerElement('spa2014-chat-panel', ChatPanelElement);
+	document.registerElement('spa2014-contacts-list', ContactsListElement);
 	document.registerElement('spa2014-recent-messages', RecentMessagesElement);
 	document.registerElement('spa2014-app', SpaApplicationElement);
 }
